bbd94936a63577362e121319c389e01e3c2d8d44
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-10-12 00:25:08 +0800

    readying data files for populating db

---
+++ src/scripts/sampleData/treatmentData.js
@@ -0,0 +1,12 @@
+// treatmentName: {
+//     type: String,
+//     required: true
+// },
+// treatmentData:{
+//     type: Date,
+//     required: true
+// },
+// status:{
+//     type: String, 
+//     enum:['ongoing', 'completed'], default: 'ongoing'
+// }
