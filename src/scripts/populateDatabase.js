0a6a3819afb3e8431bed3d6e70c0d9dd02a87f46
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-10-12 21:32:02 +0800

    populateDatabase WIP

---
+++ src/scripts/populateDatabase.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const connectToMongo = require('./connection.js');
+
+const Account = require('../models/accounts.js');
+const Patient = require('../models/patient.js');
+const Treatment = require('../models/treatment.js');
+
+const sampleAccounts = require('./sampleData/accountData.js');
+const samplePatients = require('./sampleData/patientData.js');
+const sampleTreatments = require('./sampleData/treatmentData.js');
+
+async function populateDatabase(){
+    try{
+        
+    }
+    catch{
+
+    }
+}
