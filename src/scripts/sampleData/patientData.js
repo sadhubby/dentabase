bf118ec9e58a37c280a3f3e5d4a960fe1d31c4c0
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-10-12 21:07:52 +0800

    added sample data

--- src/scripts/sampleData/patientData.js
+++ src/scripts/sampleData/patientData.js
@@ -17,3 +17,16 @@
 // },
 // patientTreatments:[treatment.schema]
 // });
+
+const samplePatient = [
+    {
+        patientID: 1,
+        patientPic: "https://static.wikia.nocookie.net/bwwe/images/5/5b/Cody_Rhodes_5.png/revision/latest?cb=20220523040407",
+        patientName: "Cody Rhodes",
+        patientContact: "1234567890",
+        patientEmail: "test_email@domain.com",
+        patientTreatments: [treatments[0]]
+    }
+];
+
+module.exports = samplePatients;
--- src/scripts/sampleData/treatmentData.js
+++ src/scripts/sampleData/treatmentData.js
@@ -10,3 +10,13 @@
 //     type: String, 
 //     enum:['ongoing', 'completed'], default: 'ongoing'
 // }
+
+const sampleTreatments = [
+    {
+        treatmentName: "Root Canal",
+        treatmentDate: new Date('2024-10-12'),
+        status: 'ongoing'
+    }
+];
+
+module.exports = sampleTreatments;
