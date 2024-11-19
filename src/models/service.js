1fbbc06a3e2afc85bc1d6abd9294b8c9b2c9624e
GutierrezAllen <allen_gutierrez@dlsu.edu.ph>
2024-11-19 22:21:43 +0800

    Added id on service

--- src/models/service.js
+++ src/models/service.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema ({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
     service: {
         type: String,
         required: true,
